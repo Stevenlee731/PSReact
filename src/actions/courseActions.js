1619656a67279172c46c82a23a3dbcd26c9efdef
stevecreated@gmail.com <stevecreated@gmail.com>
2017-06-02 02:29:21 -0700

    react redux

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,3 +1,35 @@
-export function createCourse(course) {
-  return { type: 'CREATE_COURSE', course }
+import * as types from './actionTypes'
+import courseApi from '../api/mockCourseApi'
+
+export const loadCoursesSuccess = courses => {
+  return { type: types.LOAD_COURSES_SUCCESS, courses }
+}
+
+export const updateCourseSuccess = course => {
+  return { type: types.LOAD_COURSES_SUCCESS, course }
+}
+
+export const createCourseSuccess = course => {
+  return { type: types.LOAD_COURSES_SUCCESS, course }
+}
+
+export const loadCourses = () => {
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses))
+    }).catch(error => {
+      throw(error)
+    })
+  }
+}
+
+export const saveCourse = course => {
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse))
+    }).catch(error => {
+      throw(error)
+    })
+  }
 }
